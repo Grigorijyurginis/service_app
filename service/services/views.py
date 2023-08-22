4b696ca9d799c3d8815c636f144e6fef8c338ab5
Grigory Yurginis <g.urginis@anviovr.com>
2023-08-22 14:10:32 +0300

    redis & cache

--- service/services/views.py
+++ service/services/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.cache import cache
 from django.db.models import Prefetch, F, Sum
 from django.shortcuts import render
 from rest_framework.viewsets import ReadOnlyModelViewSet
@@ -20,7 +22,15 @@ class SubscriptionView(ReadOnlyModelViewSet):
         queryset = self.filter_queryset(self.get_queryset())
         responce = super().list(request, *args, **kwargs)
 
-        total_amount = queryset.aggregate(total=Sum('price')).get('total')
+        price_cache = cache.get(settings.PRICE_CACHE_NAME)
+
+        if price_cache:
+            total_price = price_cache
+        else:
+            total_price = queryset.aggregate(total=Sum('price')).get('total')
+            cache.set(settings.PRICE_CACHE_NAME, total_price, 10)
+
+        total_amount = total_price
         responce_data = {'result': responce.data, 'total_amount': total_amount}
         responce.data = responce_data
 
